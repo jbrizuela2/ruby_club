b24b28c5151a83aba21e101b6c0beec31b7f8f2b
Juan Brizuela <juan.brizuela@michelada.io>
2019-08-29 23:06:23 -0500

    Users can now sign in and edit profile

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def edit
+    render :edit_form
+  end
+
+  def update
+    if current_user.update(user_params)
+      flash[:notice] = "User updated :D"
+      redirect_to root_path
+    else
+      flash[:alert] = "Something went wrong D:"
+      render :edit_form
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :photo)
+  end
+end
